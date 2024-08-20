1098091e1bfcb9904a234281e3e077678011caa6
MacPapo <jacopocostantini32@gmail.com>
2024-08-20 14:44:11 +0200

    Added more Translations

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,6 +44,10 @@ class User < ApplicationRecord
     med_cert_issue_date.present?
   end
 
+  def med_cert_exp_date
+    med_cert_present? ? med_cert_issue_date + 1.year : nil
+  end
+
   def get_num_of_days_til_med_cert_expire
     return -1 if med_cert_issue_date.nil?
 
