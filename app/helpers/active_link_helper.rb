1845b9d53904fa9b651b24096e80a02ecf48eb37
MacPapo <jacopocostantini32@gmail.com>
2024-09-24 23:53:08 +0200

    Updated User INDEX

--- app/helpers/active_link_helper.rb
+++ app/helpers/active_link_helper.rb
@@ -1,6 +1,8 @@
 # Active Link Helper
 module ActiveLinkHelper
   def active_link_to(text = nil, path = nil, **options, &block)
+    redirect_to new_staff_session_path unless staff_signed_in?
+
     link = block_given? ? text : path
     options[:class] = class_names(options[:class], 'fw-semibold active') if current_page? link
 
